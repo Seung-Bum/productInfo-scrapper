e8faf4a6d00d2a13e301a6b351df3bb2c7148b48
Seung-Bum <sb910126@gmail.com>
2023-10-29 20:45:11 +0900

    add headers setting value

--- scrapper.py
+++ scrapper.py
@@ -15,7 +15,13 @@ class productInfoExtract:
     # 상품 개별 index(get_detail_product에서 증가)
     idx = 0
     headers = {"User-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
-               "Accept-Language": 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7'}
+               "Accept-Language": 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+               "Accept-Encoding": 'gzip, deflate, br',
+               "Cache-Control": 'no-cache',
+               "Sec-Fetch-Site": 'same-site',
+               "Sec-Fetch-Mode": 'no-cors',
+               "Sec-Fetch-Dest": 'script',
+               }
 
     def __init__(self):
         self.str = ""
